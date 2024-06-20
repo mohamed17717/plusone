b146c4a4574de2d0f09b75cb41e363e9011f2af2
d3v.mhmd <mohamad.mahmoud@florinz.com>
2024-06-20 12:36:19 +0300

    basic serializers for the User model

---
+++ Users/serializers.py
@@ -0,0 +1,47 @@
+from django.contrib.auth import authenticate, get_user_model
+from django.utils.translation import gettext_lazy as _
+
+from rest_framework.exceptions import ValidationError
+from rest_framework import serializers
+
+
+User = get_user_model()
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        exclude = ['password', 'groups', 'user_permissions', ]
+
+    class Register(serializers.ModelSerializer):
+        class Meta:
+            model = User
+            fields = ['username', 'email',
+                      'password', 'first_name', 'last_name']
+
+        def create(self, validated_data):
+            User = self.Meta.model
+            return User.objects.create_user(**validated_data)
+
+    class Update(serializers.ModelSerializer):
+        class Meta:
+            model = User
+            exclude = [
+                'username', 'email', 'email_verified',
+                'is_staff', 'is_active', 'is_superuser',
+            ]
+
+    class Login(serializers.Serializer):
+        email = serializers.CharField(required=True)
+        password = serializers.CharField(required=True)
+
+        def validate(self, attrs):
+            request = self.context.get('request')
+            user = authenticate(request, **attrs)
+
+            if not user:
+                msg = _('Unable to log in with provided credentials.')
+                raise ValidationError({'error': msg}, code='authorization')
+
+            attrs['user'] = user
+            return attrs
