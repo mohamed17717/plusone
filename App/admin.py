9a714e7806a8a4747e9a5e9541555a1b2c6fd370
d3v.mhmd <mohamad.mahmoud@florinz.com>
2024-06-21 00:18:49 +0300

    register all models inside the admin dashboard

--- App/admin.py
+++ App/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from App import models
+
+
+admin.site.register(models.Post)
+admin.site.register(models.Category)
+admin.site.register(models.Tag)
+admin.site.register(models.Comment)
+admin.site.register(models.Vote)
