fe44b0e3dfd9e5c41375740c82bc3c3c651cb7e0
d3v.mhmd <mohamad.mahmoud@florinz.com>
2024-06-21 22:50:00 +0300

    Utils drf: - new viewset helper

--- utils/drf/viewsets.py
+++ utils/drf/viewsets.py
@@ -1,18 +1,16 @@
 from rest_framework import viewsets, mixins
 
 
-# class CRUDLViewSet(
-#         mixins.CreateModelMixin,
-#         mixins.RetrieveModelMixin,
-#         mixins.UpdateModelMixin,
-#         mixins.DestroyModelMixin,
-#         mixins.ListModelMixin,
-#         viewsets.GenericViewSet):
-#     pass
-
-
 class RetrieveUpdateViewSet(
         mixins.RetrieveModelMixin,
         mixins.UpdateModelMixin,
+        viewsets.GenericViewSet):
+    pass
+
+
+class RetrieveUpdateDeleteViewSet(
+        mixins.RetrieveModelMixin,
+        mixins.UpdateModelMixin,
+        mixins.DestroyModelMixin,
         viewsets.GenericViewSet):
     pass
