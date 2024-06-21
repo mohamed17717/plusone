8d649d67bd7891e40921a505372a3c8cd68b30f0
d3v.mhmd <mohamad.mahmoud@florinz.com>
2024-06-21 22:56:15 +0300

    PostViewset: - allow to create comment on the post - allow to list comments on the post
    Tag / Category:
    - rename the views wit hsuffix
    
    Comment:
    - use viewset to handle owner operation
    - create reply
    - list replies

--- App/views.py
+++ App/views.py
@@ -5,9 +5,12 @@ from rest_framework.generics import ListAPIView
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from rest_framework.exceptions import NotAcceptable
 from rest_framework import status
 
 from App import models, serializers
+
+from utils.drf.viewsets import RetrieveUpdateDeleteViewSet
 
 
 class PostViewSet(ModelViewSet):
@@ -90,6 +93,32 @@ class PostViewSet(ModelViewSet):
             user=request.user, post=post, defaults={'type': models.Vote.VoteType.DISLIKE})
         return Response(status=status.HTTP_204_NO_CONTENT)
 
+    @action(methods=['post'], detail=True, url_path=r'comment')
+    def comment(self, request, slug):
+        post = self.get_object()
+
+        serializer = serializers.CommentSerializer.CommentCreate(
+            data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(user=request.user, post=post)
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    @action(methods=['get'], detail=True, url_path=r'comment/list')
+    def comment_list(self, request, slug):
+        post = self.get_object()
+
+        qs = post.comments.all().filter(comment__isnull=True).select_related(
+                'user', 'user__profile')
+        serializer = serializers.CommentSerializer.CommentList
+        page = self.paginate_queryset(qs)
+        if page is not None:
+            serializer = serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = serializer(qs, many=True)
+        return Response(serializer.data)
+
     def retrieve(self, request, is_owner=False, *args, **kwargs):
         response = super().retrieve(request, *args, **kwargs)
 
@@ -101,13 +130,74 @@ class PostViewSet(ModelViewSet):
         return response
 
 
-class TagList(ListAPIView):
+class TagListAPI(ListAPIView):
     permission_classes = [AllowAny]
     serializer_class = serializers.TagSerializer
     queryset = models.Tag.objects.all()
 
 
-class CategoryList(ListAPIView):
+class CategoryListAPI(ListAPIView):
     permission_classes = [AllowAny]
     serializer_class = serializers.CategorySerializer
     queryset = models.Category.objects.all()
+
+
+class CommentViewSet(RetrieveUpdateDeleteViewSet):
+    OWNER_ACTIONS = [
+        'retrieve', 'update', 'partial_update', 'destroy',
+    ]
+    PUBLIC_ACTIONS = ['replies_list', 'reply']
+    
+
+    serializer_class = serializers.CommentSerializer
+
+    def get_permissions(self):
+        permissions = [AllowAny()]
+        if self.action in self.OWNER_ACTIONS:
+            permissions = [IsAuthenticated()]
+
+        return permissions
+
+    def get_serializer_class(self):
+        serializer_class = self.serializer_class
+
+        if self.action in self.OWNER_ACTIONS:
+            serializer_class = serializers.CommentSerializer.CommentUpdate
+        elif self.action == 'replies_list':
+            serializer_class = serializers.CommentSerializer.CommentList
+
+        return serializer_class
+
+    def get_queryset(self):
+        if self.action in self.OWNER_ACTIONS:
+            qs = self.request.user.comments.all()
+
+        elif self.action in self.PUBLIC_ACTIONS:
+            qs = models.Comment.objects.all()
+        else:
+            raise NotAcceptable('Comment not support this action')
+        return qs
+
+    @action(methods=['get'], detail=True, url_path=r'replies/list')
+    def replies_list(self, request, *args, **kwargs):
+        queryset = self.get_object().replies.all().select_related(
+                'user', 'user__profile')
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
+    @action(methods=['post'], detail=True, url_path=r'reply')
+    def reply(self, request, pk):
+        comment = self.get_object()
+
+        serializer = serializers.CommentSerializer.CommentCreate(
+            data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(user=request.user, post=comment.post, comment=comment)
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
