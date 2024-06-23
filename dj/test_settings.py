cbe9c5c01b21438653a6b5632128cf01a814ed44
d3v.mhmd <mohamad.mahmoud@florinz.com>
2024-06-23 20:12:29 +0300

    create tesing custom settings to make testing process 20x faster

---
+++ dj/test_settings.py
@@ -0,0 +1,33 @@
+
+# import the base settings
+from .settings import *
+
+# stop logging
+import logging
+logging.disable(logging.CRITICAL)
+
+# Stop debug
+DEBUG = False
+TEMPLATE_DEBUG = False
+
+
+# Weak hashing but faster
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.MD5PasswordHasher',
+]
+
+# Just keep the needed middleware
+MIDDLEWARE = [
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+]
+
+# Sqlite for simple testing
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': ':memory'
+#     }
+# }
