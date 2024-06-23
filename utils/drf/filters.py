07ece813daf21d1b94f3b00267bcece366b7a84b
d3v.mhmd <mohamad.mahmoud@florinz.com>
2024-06-23 10:23:42 +0300

    Refactor and comment on Full Text Search Class

--- utils/drf/filters.py
+++ utils/drf/filters.py
@@ -9,12 +9,16 @@ class FullTextSearchFilter(SearchFilter):
         fields = super().get_search_fields(view, request)
         if fields:
             prefixes = ''.join(self.lookup_prefixes.keys())
-            fields = list(map(lambda i: i.strip(prefixes), fields))
-
+            fields = [f.strip(prefixes) for f in fields]
         return fields
 
-    def get_raw_search_terms(self, request, search_param=None):
-        value = request.query_params.get(search_param or self.search_param, '')
+    def get_raw_search_terms(self, request, search_param):
+        # Allow mutiple search query using `,` like you search for 'python django'
+        # This will return blogs about 'python' and 'django' but if you write it like
+        # 'python, django' this will return all python blogs and all django blogs
+        # thanks for `to_raw` function that convert search query to raw query valid for FTS
+
+        value = request.query_params.get(search_param, '')
 
         field = CharField(trim_whitespace=False, allow_blank=True)
         cleaned_value = field.run_validation(value)
@@ -27,7 +31,8 @@ class FullTextSearchFilter(SearchFilter):
             def or_join(i): return ' | '.join(i)
             def and_join(i): return ' & '.join(i)
             def is_multiple(i): return ' ' in i
-            def multiple_query(i): return parentheses(and_join(map(quote, i.split(' '))))
+            def multiple_query(i): return parentheses(
+                and_join(map(quote, i.split(' '))))
 
             q = ' '.join(query)
             q = q.split('|')
@@ -40,23 +45,33 @@ class FullTextSearchFilter(SearchFilter):
             query = to_raw(query)
             return SearchQuery(query, search_type='raw')
 
-    def filter_queryset(self, request, queryset, view, distinct=True):
-        search_fields = self.get_search_fields(view, request)
-        search_terms = self.get_raw_search_terms(request, search_param=getattr(view, 'search_param', 'search'))
-        exclude_terms = self.get_raw_search_terms(request, search_param='exclude')
+    def _filter_by_exclude(self, request, queryset):
+        exclude_terms = self.get_raw_search_terms(
+            request, search_param='exclude')
+        if exclude_terms:
+            queryset = queryset.exclude(search_vector=exclude_terms)
+        return queryset
 
-        if not search_fields or (not search_terms and not exclude_terms):
-            return queryset
+    def _filter_by_include(self, request, queryset):
+        search_terms = self.get_raw_search_terms(
+            request, search_param='search')
+        if search_terms:
+            queryset = queryset.filter(search_vector=search_terms)
+        return queryset
 
-        if not hasattr(queryset.model, 'search_vector'):
+    def _setup_queryset(self, queryset, search_fields):
+        if search_fields and not hasattr(queryset.model, 'search_vector'):
             vector = SearchVector(*search_fields)
             queryset = queryset.annotate(search_vector=vector)
 
-        if search_terms:
-            queryset = queryset.filter(search_vector=search_terms)
-        if exclude_terms:
-            queryset = queryset.exclude(search_vector=exclude_terms)
-
-        if distinct:
-            return queryset.distinct()
         return queryset
+
+    def filter_queryset(self, request, queryset, view, distinct=True):
+        search_fields = self.get_search_fields(view, request)
+
+        queryset = self._setup_queryset(queryset, search_fields)
+
+        queryset = self._filter_by_include(request, queryset)
+        queryset = self._filter_by_exclude(request, queryset)
+
+        return queryset.distinct()
