3acc58aa22a65b386582f1fc12097a764793479e
d3v.mhmd <mohamad.mahmoud@florinz.com>
2024-06-23 19:57:06 +0300

    Add name to tags / categories endpoints so can use reverse method on them

--- App/urls.py
+++ App/urls.py
@@ -13,8 +13,8 @@ router.register(r'comments', views.CommentViewSet, basename='comment')
 
 
 urlpatterns = [
-    path('tags/', views.TagListAPI.as_view()),
-    path('categories/', views.CategoryListAPI.as_view()),
+    path('tags/', views.TagListAPI.as_view(), name='tag-list'),
+    path('categories/', views.CategoryListAPI.as_view(), name='category-list'),
 
     path('', include(router.urls)),
 ]
