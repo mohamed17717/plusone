cb96363f4c8351029273bcb63f841e4d65ebeb0e
d3v.mhmd <mohamad.mahmoud@florinz.com>
2024-06-23 22:23:57 +0300

    Make changes for github actions workflow

--- dj/settings.py
+++ dj/settings.py
@@ -77,16 +77,30 @@ WSGI_APPLICATION = 'dj.wsgi.application'
 
 AUTH_USER_MODEL = 'Users.User'
 
+# DATABASES = {
+#     'default': {
+#         # [Scalability] Db pool accept too many connections
+#         # better than normal 'django.db.backends.postgresql'
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': os.getenv("POSTGRES_DB"),
+#         'USER': os.getenv("POSTGRES_USER"),
+#         'PASSWORD': os.getenv("POSTGRES_PASSWORD"),
+#         'HOST': os.getenv("POSTGRES_HOST"),
+#         'PORT': os.getenv("POSTGRES_PORT", '5432'),
+#         'CONN_MAX_AGE': 600,
+#         'ATOMIC_REQUESTS': True,
+#     }
+# }
 DATABASES = {
     'default': {
         # [Scalability] Db pool accept too many connections
         # better than normal 'django.db.backends.postgresql'
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.getenv("POSTGRES_DB"),
-        'USER': os.getenv("POSTGRES_USER"),
-        'PASSWORD': os.getenv("POSTGRES_PASSWORD"),
-        'HOST': os.getenv("POSTGRES_HOST"),
-        'PORT': os.getenv("POSTGRES_PORT", '5432'),
+        'NAME': 'savvy',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': '5432',
         'CONN_MAX_AGE': 600,
         'ATOMIC_REQUESTS': True,
     }
